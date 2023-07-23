8508a8cf51ed4ad56b7166cb6ab711c777cbc45d
emmanuil <emmanuilya@gmail.com>
2023-07-23 11:03:55 +0300

    add new

--- types/dict.py
+++ types/dict.py
@@ -2,3 +2,17 @@ d = {'a':2, 'b':3}
 c = d['g'] if 'g' in d else 0
 print(type(c))# <class 'int'>
 print(c) # 0
+a = {'g':5}
+d.update(a)
+print(d) # {'a': 2, 'b': 3, 'g': 5}
+print(d.get('abdc', 'aaaa')) #aaaa
+
+sequence = ['a', 'b', 'c']
+value = 1
+
+new_dict = dict.fromkeys(sequence, value)
+
+print(new_dict)  # Выведет: {'a': 1, 'b': 1, 'c': 1}
+for k in sorted(new_dict):
+    print((k))
+print(sorted(new_dict))# ['a', 'b', 'c']
--- types/list.py
+++ types/list.py
@@ -7,3 +7,7 @@ New_M = list(map(sum, M))
 print(New_M) # [6, 15, 24]
 print(list(zip(pepe,New_M))) # [([1, 2], 6), (2, 15), ('a', 24)]
 print(dict(zip(New_M,pepe))) # {6: [1, 2], 15: 2, 24: 'a'}
+
+a = ['a', 'b', 'c']
+a[:2] = ['g', 'g']
+print(a) # ['g', 'g', 'c']
